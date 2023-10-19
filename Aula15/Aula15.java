4d387353e25abbbdf2cc9aabee6c73cf20560bd4
Lucas <Lucasluke307@gmail.com>
2023-10-19 12:58:06 -0300

    Updates

--- Aula15/Aula15.java
+++ Aula15/Aula15.java
@@ -1,19 +1,25 @@
 package Aula15;
 
+import Aula14.Usuario;
+import Aula14.Video;
+
 public class Aula15 {
 
     public static void main(String[] args){
 
         // Criando usuário
         Usuario u[] = new Usuario[2];
+        u[0] = new Usuario("João", 18, "Masculino", "John");
+        u[1] = new Usuario("Maria", 25, "Feminino", "Mariazinha");
 
         // Criando vídeo
-
         Video v[] = new Video[2];
+        v[0] = new Video("Aprenda Java em 7 Dias");
+        v[1] = new Video("Aprenda React em 7 Dias");
         
 
-        System.out.println(u1.toString());
-        System.out.println(v1.toString());
+        System.out.println(u[1].toString());
+        System.out.println(v[1].toString());
     }
     
 }
