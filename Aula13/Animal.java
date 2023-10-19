683af0d427315982139b367b4635021465be77de
Lucas <Lucasluke307@gmail.com>
2023-10-19 11:19:09 -0300

    Updates

---
+++ Aula13/Animal.java
@@ -0,0 +1,78 @@
+package Aula13;
+
+public abstract class Animal {
+    protected float peso;
+    protected int idade;
+    protected int membros;
+
+    public abstract void emitirSom();
+}
+
+class Mamifero extends Animal {
+    protected String corPelo;
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Som de Mamífero");
+    }
+}
+
+class Lobo extends Mamifero {
+
+    @Override
+    public void emitirSom() {
+        System.out.println("AUUUUUUUUUUUUUUUUUU!");
+    }
+
+}
+
+class Cachorro extends Lobo {
+
+    public void reagir(String frase) {
+        if (frase == "Toma comida" || frase == "Ola") {
+            System.out.println("Abanar e Latir");
+        } else {
+            System.out.println("Rosnar");
+        }
+    };
+
+    public void reagir(int hora, int min) {
+        if (hora < 12) {
+            System.out.println("Abanar");
+        } else if (hora >= 18) {
+            System.out.println("Ignorar");
+        } else {
+            System.out.println("Abanar e Latir");
+        }
+    };
+
+    public void reagir(boolean dono) {
+        if (dono == true) {
+            System.out.println("Abanar");
+        } else {
+            System.out.println("Rosnar e Latir");
+        }
+    };
+
+    public void reagir(int idade, float peso) {
+        if (idade < 5){
+            if (peso < 10){
+                System.out.println("Abanar");
+            } else {
+                System.out.println("Latir");
+            }
+        } else {
+            if (peso < 10){
+                System.out.println("Rosnar");
+            } else {
+                System.out.println("Ignorar");
+            }
+        }
+    };
+
+    @Override
+    public void emitirSom() {
+        System.out.println("AU! AU! AU! AU!");
+    }
+
+}
