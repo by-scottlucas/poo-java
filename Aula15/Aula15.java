b8ad94d5dee18904f88ae4079922fe91b6d45305
Lucas <Lucasluke307@gmail.com>
2023-10-19 15:31:40 -0300

    Updates

--- Aula15/Aula15.java
+++ Aula15/Aula15.java
@@ -1,11 +1,8 @@
 package Aula15;
 
-import Aula14.Usuario;
-import Aula14.Video;
-
 public class Aula15 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         // Criando usuário
         Usuario u[] = new Usuario[2];
@@ -16,10 +13,21 @@ public class Aula15 {
         Video v[] = new Video[2];
         v[0] = new Video("Aprenda Java em 7 Dias");
         v[1] = new Video("Aprenda React em 7 Dias");
-        
 
-        System.out.println(u[1].toString());
-        System.out.println(v[1].toString());
+        // Criando visualização
+        Visualizacao vis[] = new Visualizacao[2];
+        vis[0] = new Visualizacao(u[0],v[0]);
+        vis[0].avaliar();
+        System.out.println(vis[0].toString());
+
+        vis[1] = new Visualizacao(u[1], v[1]);
+        vis[1].avaliar();
+        System.out.println(35.0f);
+        System.out.println(vis[1].toString());
+
+        System.out.println(vis.toString());
+        // System.out.println(u[1].toString());
+        // System.out.println(v[1].toString());
     }
-    
+
 }
--- Aula15/Visualizacao.java
+++ Aula15/Visualizacao.java
@@ -2,35 +2,56 @@ package Aula15;
 
 public class Visualizacao {
     private Usuario espectador;
-    private Video filme;
+    private Video video;
 
-    public Visualizacao(Usuario espectador, Video filme) {
+    public Visualizacao(Usuario espectador, Video video) {
         this.espectador = espectador;
-        this.filme = filme;
+        this.video = video;
+        this.espectador.setTotAssistido(this.espectador.getTotAssistido() + 1);
+        this.video.setViews(this.video.getViews() + 1);
     }
 
+    public void avaliar() {
+        this.video.setAvaliacao(5);
+    }
+
+    public void avaliar(int nota) {
+        this.video.setAvaliacao(nota);
+    }
+
+    public void avaliar(float porcentagem) {
+        int total = 0;
+        if (porcentagem <= 20){
+            total = 3;
+        } else if (porcentagem <= 50){
+            total = 5;
+        } else if (porcentagem <= 90){
+            total = 8;
+        } else {
+            total = 10;
+        }
+        this.video.setAvaliacao(total);
+    }
 
     public Usuario getEspectador() {
         return espectador;
     }
+
     public void setEspectador(Usuario espectador) {
         this.espectador = espectador;
     }
-    public Video getFilme() {
-        return filme;
-    }
-    public void setFilme(Video filme) {
-        this.filme = filme;
+
+    public Video getVideo() {
+        return video;
     }
 
+    public void setVideo(Video video) {
+        this.video = video;
+    }
 
     @Override
     public String toString() {
-        return "\nVisualizacao \nEspectador: " + espectador + "\nFilme=" + filme + "\n";
+        return "Visualizacao [espectador=" + espectador + ", video=" + video + "]";
     }
 
-    
-
-    
-    
 }
